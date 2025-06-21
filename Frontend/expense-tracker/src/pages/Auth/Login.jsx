5af0e0b6fe2889360445f026147836d55660da0c
KanchiCodes <kanchan.gaikwad0230@gmail.com>
2025-06-21 14:43:03 +0530

    Login Page Done

--- Frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ Frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,15 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/input';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+//handle Login form submit 
+  const handleLogin = async (e) => { 
+    e.preventDefault();
+   
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address');
+      return;
+    }
+
+   
+
+    if (!password) {
+      setError('Please enter the password');
+      return;
+  }
+  setError("");
+
+  //Login API call
+  }
   return (
-    <AuthLayout> 
+    <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
         <p className='text-xs text-slate-700 mt[5px] mb-6'>
           Please enter youre details to login
         </p>
-    </div>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="kanchi@example.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Password"
+            placeholder="Min 8 characters"
+            type="password"
+          />
+
+          {error && (
+            <p className='text-red-500 text-xs pb-2.5'>{error}</p>)}
+
+          <button type="submit" className=' btn-primary '> Login </button>
+
+          <p className='text-xs text-slate-700 mt-4'>
+            Don't have an account?{' '}
+            <span
+              className='text-primary cursor-pointer'
+              onClick={() => navigate('/signUp')}
+            >
+              Sign Up
+            </span>
+          </p>
+
+        </form>
+
+      </div>
     </AuthLayout>
   );
 };
