02167ff0d8b79db7d6f0278b3d547254c3aa06dd
KanchiCodes <kanchan.gaikwad0230@gmail.com>
2025-06-23 18:47:16 +0530

    profile picture uploaded

--- Backend/server.js
+++ Backend/server.js
@@ -11,17 +11,21 @@ const app = express();
 // Middleware to handle CORS
 
 app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-             methods: ["GET", "POST", "PUT", "DELETE"],
-             allowedHeaders: ["Content-Type", "Authorization"],
-              })
-            );
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+
+
+//serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
